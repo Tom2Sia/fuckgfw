944f69f592f16c27af34c8cfe00c04e19cc2f851
Tom2Sia <yaroslavbohovchinskij@gmail.com>
2022-12-18 19:34:34 +0800

    使用go几行代码的webserver,简单替换掉nginx

--- webserver.go
+++ webserver.go
@@ -1,6 +1,6 @@
 package main
 //一个超级简单的静态服务器，使用当前路径下的  html  文件夹，提供网页服务
-//不必再安装nginx之类，使用"nohup ./webserver>>11.log& "运行，监听80端口
+//不必再安装nginx之类，使用"nohup ./webserver > logfile.txt & echo $! > pidfile.txt "运行，监听80端口
 //服务器重启后需要手动再次运行
 //结束进程使用：kill -9 `cat pidfile.txt`
 
